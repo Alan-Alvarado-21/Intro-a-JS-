572c486c13af7a947afe80567925e5f06ab0db68
Alan Alvarado <alvarado.aac@gmail.com>
2023-08-07 21:41:13 -0600

    Prueba uno, subir repo desde computadora principal

---
+++ index.js
@@ -0,0 +1,32 @@
+//Metodo de salida.
+/*
+console.log("Hola desde el JS")
+
+document.write("Hola con document.write")
+
+alert("Hola desde una alerta!")
+*/
+
+//Metodo de entrada
+
+/*
+var guardarNombre = prompt('Como te llamas?')
+
+document.write('Bienvenido ' , guardarNombre)
+*/
+/*
+var edad = prompt("ingresa tu edad")
+var nombre =prompt("ingresa tu nombre")
+var pais = prompt("ingresa tu pais")
+
+document.write("Hola soy ", nombre, " de ", pais, "y tengo ", edad, "años.")
+*/
+
+var numeroUno = Number(prompt("Ingresa el primer numero"))
+
+var numeroDos = Number(prompt("Ingresa el segundo numero"))
+
+var numeroTres = Number(prompt("Ingresa el tercer numero"))
+
+var numeroCuatro = Number(prompt("Ingresa el cuarto numero"))
+document.write(numeroUno + numeroDos +numeroTres + numeroCuatro)
